e7638a39dd57daa158c77bef33895dbb623485b9
harshit0068 <harshitsingh08062004@gmail.com>
2026-04-09 08:51:14 +0530

    [Harshit Singh] Added UC1 Welcome Page

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,18 @@
+/**
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * @author Harshit Singh
+ * @version 1.0
+ */
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        // Print the welcome message, app name, and version
+        System.out.println("=========================================");
+        System.out.println("   Welcome to the Hotel Booking App!     ");
+        System.out.println("=========================================");
+        System.out.println("Application Name: Hotel Booking Management System");
+        System.out.println("Version: v1.0");
+        System.out.println("System has started successfully.");
+    }
+}
